4904bf163986ce34d38c7371d809475f082663b0
Michael W. Lu <michaelwenlu@gmail.com>
2025-01-22 11:52:30 -0800

    Start hashtable exercises

---
+++ data-structures/hashtables/first-recurring-character.js
@@ -0,0 +1,31 @@
+//Google Question
+//Given an array = [2,5,1,2,3,5,1,2,4]:
+//It should return 2
+
+//Given an array = [2,1,1,2,3,5,1,2,4]:
+//It should return 1
+
+//Given an array = [2,3,4,5]:
+//It should return undefined
+
+function firstRecurringCharacter(input) {
+	const hashTable = new Set();
+	for (let i = 0; i < input.length; i++) {
+		if (hashTable.has(input[i])) {
+			return input[i];
+		}
+
+		hashTable.add(input[i]);
+	}
+
+	return undefined;
+}
+
+//Bonus... What if we had this:
+// [2,5,5,2,3,5,1,2,4]
+// return 5 because the pairs are before 2,2
+
+console.log(firstRecurringCharacter([2, 5, 1, 2, 3, 5, 1, 2, 4]));
+console.log(firstRecurringCharacter([2, 1, 1, 2, 3, 5, 1, 2, 4]));
+console.log(firstRecurringCharacter([2, 3, 4, 5]));
+console.log(firstRecurringCharacter([2, 5, 5, 2, 3, 5, 1, 2, 4]));
